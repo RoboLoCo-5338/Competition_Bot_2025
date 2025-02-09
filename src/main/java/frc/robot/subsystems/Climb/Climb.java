a8c901390118e27b9c8dc4e6dbdf3e6e29624963
Rohit1046 <104108284+Rohit1046@users.noreply.github.com>
2025-02-09 11:01:52 -0500

    Added climb subsystem

--- src/main/java/frc/robot/subsystems/Climb/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb/Climb.java
@@ -1,20 +1,56 @@
 package frc.robot.subsystems.Climb;
 
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.Mode;
+
 import org.littletonrobotics.junction.Logger;
 
 public class Climb extends SubsystemBase {
 
   private final ClimbIO climbIO;
-  private final ClimbIOInputsAutoLogged climbInputs = new ClimbIOInputsAutoLogged();
+  private final ClimbIOInputsAutoLogged inputs = new ClimbIOInputsAutoLogged();
+  private final Alert climbMotorDisconnectedAlert = new Alert("Climb Motor Disconnected", AlertType.kError);
 
   public Climb(ClimbIO io) {
     this.climbIO = io;
   }
 
+  /**
+   * Periodic function for the Climb subsystem. Updates the inputs and alerts the driver if the
+   * climb motor is disconnected and the robot is in real mode.
+   */
   @Override
   public void periodic() {
-    climbIO.updateInputs(climbInputs);
-    Logger.processInputs("Climb", climbInputs);
+    climbIO.updateInputs(inputs);
+    Logger.processInputs("Climb", inputs);
+
+    climbMotorDisconnectedAlert.set(!inputs.climbConnected && Constants.currentMode!= Mode.SIM);
+  }
+
+/**
+ * Creates an InstantCommand that sets the climb motor to the specified position in radians.
+ *
+ * @param position The target position for the climb motor in radians.
+ * @return An InstantCommand that sets the climb motor to the specified position.
+ */
+
+  public Command setClimbPosition(double position) {
+    return new InstantCommand( ()-> climbIO.setClimbPosition(position));
+  }
+
+  /**
+   * Creates an InstantCommand that sets the climb motor to the specified velocity in radians per
+   * second.
+   *
+   * @param velocity The target velocity for the climb motor in radians per second.
+   * @return An InstantCommand that sets the climb motor to the specified velocity.
+   */
+  public Command setClimbVelocity(double velocity) {
+    return new InstantCommand( ()-> climbIO.setClimbVelocity(velocity));
   }
 }
