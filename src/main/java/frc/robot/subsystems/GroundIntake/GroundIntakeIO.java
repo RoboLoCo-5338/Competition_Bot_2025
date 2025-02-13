f947f3e6f829dc8cf81afbca9df9b31f8af37a28
Rohit1046 <104108284+Rohit1046@users.noreply.github.com>
2025-02-13 17:03:30 -0500

    Ground intake down other than sim

--- src/main/java/frc/robot/subsystems/GroundIntake/GroundIntakeIO.java
+++ src/main/java/frc/robot/subsystems/GroundIntake/GroundIntakeIO.java
@@ -1,9 +1,27 @@
 package frc.robot.subsystems.GroundIntake;
 
-public interface GroundIntakeIO {
+import org.littletonrobotics.junction.AutoLog;
 
-    public static class GroundIntakeIOInputs{
-        
-    }
-    
+public interface GroundIntakeIO {
+  @AutoLog
+  public static class GroundIntakeIOInputs {
+    public boolean armMotorConnected = false;
+    public double armPositionRad = 0.0;
+    public double armVelocityRadPerSec = 0.0;
+    public double armAppliedVolts = 0.0;
+    public double armCurrentAmps = 0.0;
+
+    public boolean intakeMotorConnected = false;
+    public double intakeVelocityRadPerSec = 0.0;
+    public double intakeAppliedVolts = 0.0;
+    public double intakeCurrentAmps = 0.0;
+  }
+
+  public default void updateInputs(GroundIntakeIOInputs inputs) {}
+
+  public default void setArmVelocity(double velocityRadPerSec) {}
+
+  public default void setArmPosition(double position) {}
+
+  public default void setIntakeVelocity(double velocityRadPerSec) {}
 }
