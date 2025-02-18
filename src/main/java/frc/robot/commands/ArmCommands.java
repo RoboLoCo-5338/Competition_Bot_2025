55b6b2b84961b035673aea985a747c7befeb09e7
Kavin Muralikrishnan <921563@lcps.org>
2025-02-18 12:27:28 -0500

    package rename

--- src/main/java/frc/robot/commands/ArmCommands.java
+++ src/main/java/frc/robot/commands/ArmCommands.java
@@ -3,8 +3,8 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import frc.robot.subsystems.Arm.Arm;
-import frc.robot.subsystems.EndEffector.EndEffector;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.endeffector.EndEffector;
 
 public class ArmCommands {
 
