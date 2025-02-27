b66eabad25fb609134e92ebf5429ae9707237d73
iron <66088066+ironon@users.noreply.github.com>
2025-02-27 17:48:40 -0500

    refactored code - tested, still works

---
+++ src/main/java/frc/robot/ActionBindings.java
@@ -0,0 +1,124 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.ArmCommands;
+import frc.robot.commands.ClimbCommands;
+import frc.robot.commands.DriveCommands;
+import frc.robot.commands.ElevatorCommands;
+import frc.robot.commands.EndEffectorCommands;
+import frc.robot.commands.GroundIntakeCommands;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.climb.Climb;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.endeffector.EndEffector;
+import frc.robot.subsystems.groundintake.GroundIntake;
+import frc.robot.subsystems.led.LED;
+
+public class ActionBindings {
+  public static Drive drive;
+  public static CommandXboxController driveController;
+  public static CommandXboxController operatorController;
+  public static double exponentialVariable = 25.0;
+  public static Climb climb;
+  public static Elevator elevator;
+  public static GroundIntake groundIntake;
+  public static EndEffector endEffector;
+  public static LED led;
+  public static Arm arm;
+
+  public static Command lockToZero() {
+    return DriveCommands.joystickDriveAtAngle(
+        drive,
+        () -> -driveController.getLeftY(),
+        () -> -driveController.getLeftX(),
+        () -> new Rotation2d());
+  }
+
+  public static Command climbPreset() {
+    return ButtonBindings.blankCommand("climbPreset");
+  }
+
+  public static Command gyroReset() {
+    return Commands.runOnce(
+            () -> drive.setPose(new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
+            drive)
+        .ignoringDisable(true);
+  }
+
+  public static Command manualClimbDown() {
+    return ClimbCommands.moveClimbArm(climb, -0.4);
+  }
+
+  public static Command manualClimbUp() {
+    return ClimbCommands.moveClimbArm(climb, 0.4);
+  }
+
+  public static Command manualElevatorUp() {
+    return ElevatorCommands.moveElevator(elevator, 0.4);
+  }
+
+  public static Command manualElevatorDown() {
+    return ElevatorCommands.moveElevator(elevator, -0.4);
+  }
+
+  public static Command l3Preset() {
+    return ButtonBindings.blankCommand("l3Preset");
+  }
+
+  public static Command elevatorSlow() {
+    return ButtonBindings.blankCommand("elevatorSlow");
+  }
+
+  public static Command l2Preset() {
+    return ButtonBindings.blankCommand("l2Preset");
+  }
+
+  public static Command l4Preset() {
+    return ButtonBindings.blankCommand("l4Preset");
+  }
+
+  public static Command elevatorFast() {
+    return ButtonBindings.blankCommand("elevatorFast");
+  }
+
+  public static Command manualArmDown() {
+    return ArmCommands.moveArm(arm, -0.4);
+  }
+
+  public static Command manualArmUp() {
+    return ArmCommands.moveArm(arm, 0.4);
+  }
+
+  public static Command netPreset() {
+    return ButtonBindings.blankCommand("netPreset");
+  }
+
+  public static Command groundIntakeIn() {
+    return GroundIntakeCommands.moveGroundIntakeArm(groundIntake, -0.4, 0.4);
+  }
+
+  public static Command groundIntakeOut() {
+    return GroundIntakeCommands.moveGroundIntakeArm(groundIntake, 0.4, -0.4);
+  }
+
+  public static Command endEffectorIn() {
+    return EndEffectorCommands.moveEndEffector(endEffector, 0.4);
+  }
+
+  public static Command endEffectorOut() {
+    return EndEffectorCommands.moveEndEffector(endEffector, -0.4);
+  }
+
+  public static Command groundIntakeSlow() {
+    return ButtonBindings.blankCommand("groundIntakeSlow");
+  }
+
+  public static Command groundIntakeFast() {
+    return ButtonBindings.blankCommand("groundIntakeFast");
+  }
+}
--- src/main/java/frc/robot/ButtonBindings.java
+++ src/main/java/frc/robot/ButtonBindings.java
@@ -1,19 +1,13 @@
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
+import static java.util.Map.entry;
+
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.ArmCommands;
-import frc.robot.commands.ClimbCommands;
 import frc.robot.commands.DriveCommands;
-import frc.robot.commands.ElevatorCommands;
-import frc.robot.commands.EndEffectorCommands;
-import frc.robot.commands.GroundIntakeCommands;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.climb.Climb;
 import frc.robot.subsystems.drive.Drive;
@@ -21,7 +15,7 @@ import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.endeffector.EndEffector;
 import frc.robot.subsystems.groundintake.GroundIntake;
 import frc.robot.subsystems.led.LED;
-import java.util.HashMap;
+import java.util.Map;
 
 public class ButtonBindings {
   private Drive drive;
@@ -37,14 +31,14 @@ public class ButtonBindings {
   private double exponentialVariable = 25.0;
 
   // maps button names and function names to their respective objects
-  public HashMap<String, Trigger> buttonMappings = new HashMap<>();
-  public HashMap<String, Command> functionBindings = new HashMap<>();
+  public Map<String, Trigger> buttonMappings;
+  public Map<String, Command> functionBindings;
 
   // these hashmaps are what happens when the button is pressed
-  public HashMap<String, String> buttonToFunction = new HashMap<>();
+  public Map<String, String> buttonToFunction;
 
   // these hashmaps are what happens when the button is not pressed
-  public HashMap<String, String> negatedButtonToFunction = new HashMap<>();
+  public Map<String, String> negatedButtonToFunction;
 
   public ButtonBindings(
       Drive drive,
@@ -55,12 +49,14 @@ public class ButtonBindings {
       Climb climb,
       Arm arm) {
     this.drive = drive;
-    this.led = led;
-    this.elevator = elevator;
-    this.groundIntake = groundIntake;
-    this.endEffector = endEffector;
-    this.climb = climb;
-    this.arm = arm;
+    ActionBindings.drive = drive;
+    ActionBindings.arm = arm;
+    ActionBindings.led = led;
+    ActionBindings.elevator = elevator;
+    ActionBindings.groundIntake = groundIntake;
+    ActionBindings.endEffector = endEffector;
+    ActionBindings.climb = climb;
+    ActionBindings.arm = arm;
 
     setUpFunctionBindings();
     setUpButtonMappings();
@@ -69,100 +65,100 @@ public class ButtonBindings {
     defaultButtonBindings(); // handles any input coming from joysticks
   }
 
+  @SuppressWarnings({"rawtypes", "unchecked"})
   private void setUpFunctionBindings() {
-    functionBindings.put("Lock to Zero", this.lockToZero());
-    functionBindings.put("Climb Preset", this.climbPreset());
-    functionBindings.put("Gyro Reset", this.gyroReset());
-    functionBindings.put("Manual Climb Down", this.manualClimbDown());
-    functionBindings.put("Manual Climb Up", this.manualClimbUp());
-    functionBindings.put("L3 Preset", this.l3Preset());
-    functionBindings.put("Elevator Slow", this.elevatorSlow());
-    functionBindings.put("L2 Preset", this.l2Preset());
-    functionBindings.put("L4 Preset", this.l4Preset());
-    functionBindings.put("Elevator Fast", this.elevatorFast());
-    functionBindings.put("Manual Arm Down", this.manualArmDown());
-    functionBindings.put("Manual Arm Up", this.manualArmUp());
-    functionBindings.put("Net Preset", this.netPreset());
-    functionBindings.put("Ground Intake In", this.groundIntakeIn());
-    functionBindings.put("Ground Intake Out", this.groundIntakeOut());
-    functionBindings.put("End Effector In", this.endEffectorIn());
-    functionBindings.put("End Effector Out", this.endEffectorOut());
-    functionBindings.put("Ground Intake Slow", this.groundIntakeSlow());
-    functionBindings.put("Ground Intake Fast", this.groundIntakeFast());
-    functionBindings.put("Elevator Up", this.manualElevatorUp());
-    functionBindings.put("Elevator Down", this.manualElevatorDown());
-    functionBindings.put("Arm Up", this.manualArmUp());
-    functionBindings.put("Arm Down", this.manualArmDown());
+
+    functionBindings =
+        Map.ofEntries(
+            entry("Lock to Zero", ActionBindings.lockToZero()),
+            entry("Climb Preset", ActionBindings.climbPreset()),
+            entry("Gyro Reset", ActionBindings.gyroReset()),
+            entry("Manual Climb Down", ActionBindings.manualClimbDown()),
+            entry("Manual Climb Up", ActionBindings.manualClimbUp()),
+            entry("L3 Preset", ActionBindings.l3Preset()),
+            entry("Elevator Slow", ActionBindings.elevatorSlow()),
+            entry("L2 Preset", ActionBindings.l2Preset()),
+            entry("L4 Preset", ActionBindings.l4Preset()),
+            entry("Elevator Fast", ActionBindings.elevatorFast()),
+            entry("Manual Arm Down", ActionBindings.manualArmDown()),
+            entry("Manual Arm Up", ActionBindings.manualArmUp()),
+            entry("Net Preset", ActionBindings.netPreset()),
+            entry("Ground Intake In", ActionBindings.groundIntakeIn()),
+            entry("Ground Intake Out", ActionBindings.groundIntakeOut()),
+            entry("End Effector In", ActionBindings.endEffectorIn()),
+            entry("End Effector Out", ActionBindings.endEffectorOut()),
+            entry("Ground Intake Slow", ActionBindings.groundIntakeSlow()),
+            entry("Ground Intake Fast", ActionBindings.groundIntakeFast()),
+            entry("Elevator Up", ActionBindings.manualElevatorUp()),
+            entry("Elevator Down", ActionBindings.manualElevatorDown()),
+            entry("Arm Up", ActionBindings.manualArmUp()),
+            entry("Arm Down", ActionBindings.manualArmDown()));
   }
 
+  @SuppressWarnings("unchecked")
   private void setUpButtonMappings() {
-    buttonMappings.put("D - x", driveController.x());
-    buttonMappings.put("D - a", driveController.a());
-    buttonMappings.put("D - y", driveController.y());
-    buttonMappings.put("D - b", driveController.b());
-    buttonMappings.put("D - Right Joystick Button", driveController.rightStick());
-    buttonMappings.put("D - Left Joystick Button", driveController.leftStick());
-    buttonMappings.put("D - Left Bumper", driveController.leftBumper());
-    buttonMappings.put("D - Left Trigger", driveController.leftTrigger());
-    buttonMappings.put("D - Right Bumper", driveController.rightBumper());
-    buttonMappings.put("D - D-Pad Up", driveController.povUp());
-    buttonMappings.put("D - D-Pad Down", driveController.povDown());
-    buttonMappings.put("D - D-Pad Left", driveController.povLeft());
-    buttonMappings.put("D - D-Pad Right", driveController.povRight());
-
-    buttonMappings.put(
-        "D - Right Trigger",
-        new Trigger(
-            () ->
-                driveController.getRawAxis(5)
-                    > -0.5)); // the .rightTrigger() trigger does not work for me - maybe its a
-    // linux thing - or WPILib is slightly broken
-
-    buttonMappings.put("O - a", operatorController.a());
-    buttonMappings.put("O - y", operatorController.y());
-    buttonMappings.put("O - B", operatorController.b());
-    buttonMappings.put("O - Right Bumper", operatorController.rightBumper());
-    buttonMappings.put("O - Left Bumper", operatorController.leftBumper());
-    buttonMappings.put(
-        "O - Right Trigger", new Trigger(() -> operatorController.getRawAxis(5) > -0.5));
-    buttonMappings.put("O - Left Trigger", operatorController.leftTrigger());
-    buttonMappings.put("O - D-Pad Down", operatorController.povDown());
-    buttonMappings.put("O - D-Pad Up", operatorController.povUp());
-    buttonMappings.put("O - D-Pad Left", operatorController.povLeft());
+    buttonMappings =
+        Map.ofEntries(
+            entry("D - x", driveController.x()),
+            entry("D - a", driveController.a()),
+            entry("D - y", driveController.y()),
+            entry("D - b", driveController.b()),
+            entry("D - Right Joystick Button", driveController.rightStick()),
+            entry("D - Left Joystick Button", driveController.leftStick()),
+            entry("D - Left Bumper", driveController.leftBumper()),
+            entry("D - Left Trigger", driveController.leftTrigger()),
+            entry("D - Right Bumper", driveController.rightBumper()),
+            entry("D - D-Pad Up", driveController.povUp()),
+            entry("D - D-Pad Down", driveController.povDown()),
+            entry("D - D-Pad Left", driveController.povLeft()),
+            entry("D - D-Pad Right", driveController.povRight()),
+            entry("D - Right Trigger", new Trigger(() -> driveController.getRawAxis(5) > -0.5)),
+            entry("O - a", operatorController.a()),
+            entry("O - y", operatorController.y()),
+            entry("O - B", operatorController.b()),
+            entry("O - Right Bumper", operatorController.rightBumper()),
+            entry("O - Left Bumper", operatorController.leftBumper()),
+            entry("O - Right Trigger", new Trigger(() -> operatorController.getRawAxis(5) > -0.5)),
+            entry("O - Left Trigger", operatorController.leftTrigger()),
+            entry("O - D-Pad Down", operatorController.povDown()),
+            entry("O - D-Pad Up", operatorController.povUp()),
+            entry("O - D-Pad Left", operatorController.povLeft()));
   }
 
+  @SuppressWarnings({"rawtypes", "unchecked"})
   private void setUpButtonBindings() {
     // Driver Controller Bindings
-    buttonToFunction.put("D - x", "Lock to Zero");
-    buttonToFunction.put("D - a", "Climb Preset");
-    buttonToFunction.put("D - y", "Gyro Reset");
-    buttonToFunction.put("D - Right Joystick Button", "Manual Climb Down");
-    buttonToFunction.put("D - Left Joystick Button", "Manual Climb Up");
-    buttonToFunction.put("D - Left Bumper", "L3 Preset");
-    buttonToFunction.put("D - Left Trigger", "Elevator Slow");
-    buttonToFunction.put("D - Right Bumper", "L2 Preset");
-    buttonToFunction.put("D - Right Trigger", "L4 Preset");
-    buttonToFunction.put("D - D-Pad Up", "Elevator Up");
-    buttonToFunction.put("D - D-Pad Down", "Elevator Down");
-    buttonToFunction.put("D - D-Pad Left", "Arm Up");
-    buttonToFunction.put("D - D-Pad Right", "Arm Down");
-    // Negated Driver Controller Bindings
-    negatedButtonToFunction.put("D - Left Trigger", "Elevator Fast");
-
-    // Operator Controller Bindings
-    buttonToFunction.put("O - a", "Manual Arm Down");
-    buttonToFunction.put("O - y", "Manual Arm Up");
-    buttonToFunction.put("O - b", "Net Preset");
-    buttonToFunction.put("O - Right Bumper", "Ground Intake In");
-    buttonToFunction.put("O - Left Bumper", "Ground Intake Out");
-    buttonToFunction.put("O - Right Trigger", "End Effector In");
-    buttonToFunction.put("O - Left Trigger", "End Effector Out");
-    buttonToFunction.put("O - D-Pad Down", "Ground Intake In");
-    buttonToFunction.put("O - D-Pad Up", "Ground Intake Slow");
-    buttonToFunction.put("O - D-Pad Left", "Ground Intake Out");
+    buttonToFunction =
+        Map.ofEntries(
+            entry("D - x", "Lock to Zero"),
+            entry("D - a", "Climb Preset"),
+            entry("D - y", "Gyro Reset"),
+            entry("D - Right Joystick Button", "Manual Climb Down"),
+            entry("D - Left Joystick Button", "Manual Climb Up"),
+            entry("D - Left Bumper", "L3 Preset"),
+            entry("D - Left Trigger", "Elevator Slow"),
+            entry("D - Right Bumper", "L2 Preset"),
+            entry("D - Right Trigger", "L4 Preset"),
+            entry("D - D-Pad Up", "Elevator Up"),
+            entry("D - D-Pad Down", "Elevator Down"),
+            entry("D - D-Pad Left", "Arm Up"),
+            entry("D - D-Pad Right", "Arm Down"),
+            entry("O - a", "Manual Arm Down"),
+            entry("O - y", "Manual Arm Up"),
+            entry("O - b", "Net Preset"),
+            entry("O - Right Bumper", "Ground Intake In"),
+            entry("O - Left Bumper", "Ground Intake Out"),
+            entry("O - Right Trigger", "End Effector In"),
+            entry("O - Left Trigger", "End Effector Out"),
+            entry("O - D-Pad Down", "Ground Intake In"),
+            entry("O - D-Pad Up", "Ground Intake Slow"),
+            entry("O - D-Pad Left", "Ground Intake Out"));
 
     // Negated Operator Controller Bindings
-    negatedButtonToFunction.put("O - D-Pad Up", "Ground Intake Fast");
+    negatedButtonToFunction =
+        Map.ofEntries(
+            entry("O - D-Pad Up", "Ground Intake Fast"),
+            entry("D - Left Trigger", "Elevator Fast"));
   }
 
   public static void periodic() {}
@@ -239,96 +235,5 @@ public class ButtonBindings {
         () -> {
           System.out.println(name + " is not implemented!");
         });
-  }
-
-  public Command lockToZero() {
-    return DriveCommands.joystickDriveAtAngle(
-        drive,
-        () -> -driveController.getLeftY(),
-        () -> -driveController.getLeftX(),
-        () -> new Rotation2d());
-  }
-
-  public Command climbPreset() {
-    return ButtonBindings.blankCommand("climbPreset");
-  }
-
-  public Command gyroReset() {
-    return Commands.runOnce(
-            () -> drive.setPose(new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
-            drive)
-        .ignoringDisable(true);
-  }
-
-  public Command manualClimbDown() {
-    return ClimbCommands.moveClimbArm(climb, -0.4);
-  }
-
-  public Command manualClimbUp() {
-    return ClimbCommands.moveClimbArm(climb, 0.4);
-  }
-
-  public Command manualElevatorUp() {
-    return ElevatorCommands.moveElevator(elevator, 0.4);
-  }
-
-  public Command manualElevatorDown() {
-    return ElevatorCommands.moveElevator(elevator, -0.4);
-  }
-
-  public Command l3Preset() {
-    return ButtonBindings.blankCommand("l3Preset");
-  }
-
-  public Command elevatorSlow() {
-    return ButtonBindings.blankCommand("elevatorSlow");
-  }
-
-  public Command l2Preset() {
-    return ButtonBindings.blankCommand("l2Preset");
-  }
-
-  public Command l4Preset() {
-    return ButtonBindings.blankCommand("l4Preset");
-  }
-
-  public Command elevatorFast() {
-    return ButtonBindings.blankCommand("elevatorFast");
-  }
-
-  public Command manualArmDown() {
-    return ArmCommands.moveArm(arm, -0.4);
-  }
-
-  public Command manualArmUp() {
-    return ArmCommands.moveArm(arm, 0.4);
-  }
-
-  public Command netPreset() {
-    return ButtonBindings.blankCommand("netPreset");
-  }
-
-  public Command groundIntakeIn() {
-    return GroundIntakeCommands.moveGroundIntakeArm(groundIntake, -0.4, 0.4);
-  }
-
-  public Command groundIntakeOut() {
-    return GroundIntakeCommands.moveGroundIntakeArm(groundIntake, 0.4, -0.4);
-  }
-
-  public Command endEffectorIn() {
-    return EndEffectorCommands.moveEndEffector(endEffector, 0.4);
-  }
-
-  public Command endEffectorOut() {
-    return EndEffectorCommands.moveEndEffector(endEffector, -0.4);
-  }
-
-  public Command groundIntakeSlow() {
-    return ButtonBindings.blankCommand("groundIntakeSlow");
-  }
-
-  public Command groundIntakeFast() {
-    return ButtonBindings.blankCommand("groundIntakeFast");
   }
 }
