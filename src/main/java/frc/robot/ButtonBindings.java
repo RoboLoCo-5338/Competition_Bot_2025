688398a754fc7364f7f3d1c0c07d68dd3810058b
RoboLoCo, Team 5338 <team5338@gmail.com>
2025-03-08 16:46:27 -0500

    Update at 'Sat Mar 08 16:46:26 EST 2025'

--- src/main/java/frc/robot/ButtonBindings.java
+++ src/main/java/frc/robot/ButtonBindings.java
@@ -227,7 +227,7 @@ public class ButtonBindings {
             drive,
             () -> -driveController.getLeftY(),
             () -> -driveController.getLeftX(),
-            () -> -driveController.getRightX() / 1.25));
+            () -> -Math.pow(driveController.getRightX(),2) ));
     // elevator.setDefaultCommand(elevator.setElevatorVelocity(operatorController.getLeftY()));
     // handles translation and rotation driving (i think!?)
   }
