80a35f1d2239921d7e186b63464e1a6f9bc62620
RoboLoCo, Team 5338 <team5338@gmail.com>
2025-03-09 09:24:06 -0400

    Update at 'Sun Mar 09 09:24:06 EDT 2025'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,7 +63,7 @@ public final class Constants {
     public static final int ARM_MOTOR_ID = 44;
     public static final double ARM_MOTOR_KP = 3.5;
     public static final double ARM_MOTOR_KI = 0.01;
-    public static final double ARM_MOTOR_KD = 1.4;
+    public static final double ARM_MOTOR_KD = 2.8;
     public static final double ARM_MOTOR_KG = 0.0;
     public static final double ARM_MOTOR_KV = 0.00;
     public static final double ARM_MOTOR_KFF = 0.0;
