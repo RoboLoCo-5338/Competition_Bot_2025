c7d2647244ca36ea8c21354ccbec204fd0629941
RoboLoCo, Team 5338 <team5338@gmail.com>
2025-03-08 19:08:08 -0500

    Update at 'Sat Mar 08 19:08:08 EST 2025'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -116,8 +116,8 @@ public final class Constants {
 
     public static final class ElevatorVelocityConstants {
       public static final double ELEVATOR_FEEDFORWARD = 0.0;
-      public static final double ELEVATOR_MOTOR_kP = 0.15; // 0.3
-      public static final double ELEVATOR_MOTOR_kI = 0.03;
+      public static final double ELEVATOR_MOTOR_kP = 0.00; // 0.15
+      public static final double ELEVATOR_MOTOR_kI = 0.00; //0.03
       public static final double ELEVATOR_MOTOR_kD = 0.00;
       public static final double ELEVATOR_MOTOR_kG = 0.35; // 0.5
       public static final double ELEVATOR_MOTOR_kV = 0.0;
