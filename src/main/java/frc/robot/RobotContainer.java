51220c2a5425188e918e7dc19414d0df3f04c57b
iron <66088066+ironon@users.noreply.github.com>
2025-03-20 18:55:51 -0400

    fixed bug

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -129,7 +129,7 @@ public class RobotContainer {
         climb = new Climb(new ClimbIOTalonFX());
         arm = new Arm(new ArmIOSpark());
         led = new LED(new AddressableLEDIO());
-        led.setBargeIndicator(drive, endEffector);
+        led.setBargeIndicator(drive, elevator);
         ButtonBindingsController =
             new ButtonBindings(drive, led, elevator, groundIntake, endEffector, climb, arm);
 
