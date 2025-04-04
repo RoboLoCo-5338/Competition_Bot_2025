daeff530bd220841cd13f5d26f51e0bfe7e66373
RoboLoCo, Team 5338 <team5338@gmail.com>
2025-04-04 14:37:10 -0400

    Update at 'Fri Apr 04 14:37:09 EDT 2025'

--- src/main/java/frc/robot/commands/PresetCommands.java
+++ src/main/java/frc/robot/commands/PresetCommands.java
@@ -21,7 +21,7 @@ public class PresetCommands {
 
   public static Command stowElevator(Elevator elevator, EndEffector endEffector, Arm arm) {
     return new SequentialCommandGroup(
-        arm.setArmPosition(0.54),
+        arm.setArmPosition(0.54), 
         new WaitCommand(0.3),
         elevator.setElevatorPosition(0.05, 2),
         arm.setArmPosition(0.5));
@@ -36,21 +36,21 @@ public class PresetCommands {
 
     SmartDashboard.putString("preset2", "inside preset functoin");
     return new SequentialCommandGroup(
-        endEffectorSet(endEffector, arm, 0.61),
+        endEffectorSet(endEffector, arm, 0.54),
         new WaitCommand(0.3),
         elevator.setElevatorPosition(PresetConstants.elevatorl2, 0));
   }
 
   public static Command presetL3(Elevator elevator, EndEffector endEffector, Arm arm) {
     return new SequentialCommandGroup(
-        endEffectorSet(endEffector, arm, 0.61),
+        endEffectorSet(endEffector, arm, 0.54),
         new WaitCommand(0.3),
         elevator.setElevatorPosition(PresetConstants.elevatorl3, 0));
   }
 
   public static Command presetL4(Elevator elevator, EndEffector endEffector, Arm arm) {
     return new SequentialCommandGroup(
-        endEffectorSet(endEffector, arm, 0.61),
+        endEffectorSet(endEffector, arm, 0.54),
         new ParallelCommandGroup(
             arm.setArmPosition(PresetConstants.arml4),
             elevator.setElevatorPosition(PresetConstants.elevatorl4, 0)));
