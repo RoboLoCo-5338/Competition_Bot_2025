9d86bdcf3fe767e938d7704d3a90ab0b661f7571
RoboLoCo, Team 5338 <team5338@gmail.com>
2025-04-06 09:58:12 -0400

    Update at 'Sun Apr 06 09:58:12 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,6 +25,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -384,7 +385,10 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return autoChooser.get();
+    return new SequentialCommandGroup(
+        PresetCommands.stopAll(elevator, endEffector, arm),
+        autoChooser.get()
+    );
   }
 
   public RunCommand startRainbow() {
