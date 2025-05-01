2405d48e48f8d868aa3eeea143af55a6d26cdb8a
Kavin Muralikrishnan <921563@lcps.org>
2025-05-01 18:19:01 -0400

    Update ArmConstants.java

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -5,13 +5,13 @@ import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 
 public final class ArmConstants {
   public static final int ARM_MOTOR_ID = 44;
-  public static final double ARM_MOTOR_POSITION_KP = 2.5;
+  public static final double ARM_MOTOR_POSITION_KP = 0.0;
   public static final double ARM_MOTOR_POSITION_KI = 0.00;
-  public static final double ARM_MOTOR_POSITION_KD = 0.25;
-  public static final double ARM_MOTOR_VELOCITY_KP = 0.005;
+  public static final double ARM_MOTOR_POSITION_KD = 0.0;
+  public static final double ARM_MOTOR_VELOCITY_KP = 0.00;
   public static final double ARM_MOTOR_VELOCITY_KI = 0.00;
-  public static final double ARM_MOTOR_VELOCITY_KD = 0.0005;
-  public static final double ARM_MOTOR_KG = 0.25;
+  public static final double ARM_MOTOR_VELOCITY_KD = 0.000;
+  public static final double ARM_MOTOR_KG = 0.0;
   public static final double ARM_MOTOR_KV = 0.00;
   public static final double ARM_MOTOR_KS = 0.0;
   public static final int ARM_MOTOR_CURRENT_LIMIT = 60;
