44af11cb8d19f0871ec8800ff7a2025cfcb026a4
Suha Nalluri <nallurisuha@gmail.com>
2025-06-07 15:44:53 -0400

    Removed endeffector parameters from preset commands.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -145,8 +145,8 @@ public class RobotContainer {
     }
 
     // Set up commands for auto
-    NamedCommands.registerCommand("L4 Preset", PresetCommands.presetL4(elevator, endEffector, arm));
-    NamedCommands.registerCommand("L2 Preset", PresetCommands.presetL2(elevator, endEffector, arm));
+    NamedCommands.registerCommand("L4 Preset", PresetCommands.presetL4(elevator, arm));
+    NamedCommands.registerCommand("L2 Preset", PresetCommands.presetL2(elevator, arm));
 
     NamedCommands.registerCommand("Endeffector Out", endEffector.setEndEffectorVelocity(100));
     NamedCommands.registerCommand("Endeffector Out L4", endEffector.setEndEffectorVelocity(-100));
@@ -162,7 +162,7 @@ public class RobotContainer {
     NamedCommands.registerCommand(
         "Stop Preset", PresetCommands.stopAll(elevator, endEffector, arm));
     NamedCommands.registerCommand(
-        "StowPreset", PresetCommands.stowElevator(elevator, endEffector, arm));
+        "StowPreset", PresetCommands.stowElevator(elevator, arm));
 
     // Set up auto routines
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
@@ -263,10 +263,10 @@ public class RobotContainer {
         .whileTrue(endEffector.setEndEffectorVelocity(-100))
         .onFalse(endEffector.setEndEffectorVelocity(0));
 
-    operatorController.a().onTrue(PresetCommands.stowElevator(elevator, endEffector, arm));
-    operatorController.b().whileTrue(PresetCommands.presetL2(elevator, endEffector, arm));
-    operatorController.x().whileTrue(PresetCommands.presetL3(elevator, endEffector, arm));
-    operatorController.y().whileTrue(PresetCommands.presetL4(elevator, endEffector, arm));
+    operatorController.a().onTrue(PresetCommands.stowElevator(elevator, arm));
+    operatorController.b().whileTrue(PresetCommands.presetL2(elevator, arm));
+    operatorController.x().whileTrue(PresetCommands.presetL3(elevator, arm));
+    operatorController.y().whileTrue(PresetCommands.presetL4(elevator, arm));
 
     operatorController
         .rightBumper()
